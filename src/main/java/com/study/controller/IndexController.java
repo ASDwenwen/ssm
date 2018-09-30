ef07d1f58232d399641753ad02d2a80152fb64b5
tan <1.com>
2018-09-30 13:55:16 +0800

    测试

--- src/main/java/com/study/controller/IndexController.java
+++ src/main/java/com/study/controller/IndexController.java
@@ -23,71 +23,14 @@ import java.util.Properties;
 /**
  * Administrator
  */
-@Controller
-//@RestController//组合注解@Controller @ResponseBody
+@RestController//组合注解@Controller @ResponseBody
 public class IndexController {
 
-    @Autowired
-    private UserService userService;
-    @Autowired
-    private FreeMarkerConfigurer freeMarkerConfigurer;
 
-    @GetMapping(value="/"/*,produces = "text/html;charset=utf-8"*/)
-    //@ResponseBody
+    @GetMapping(value={"/","/index"})
     public String index(Model model, HttpServletRequest request){
-        /*User user=new User();
-        user.setUsername("tan");
-        user.setPassword("123");
-        user.setEmail("138");
-        user.setPhone("111");
-        userService.add(user);*/
-        return "index";
-/*//        response.setContentType("text/html;charset=utf-8");
-        Configuration configuration = freeMarkerConfigurer.getConfiguration();
-        Template template = null;
-        String string = null;
-        try {
-             template = configuration.getTemplate("index.ftl");
-            try {
-                 string = FreeMarkerTemplateUtils.processTemplateIntoString(template, model);
-            } catch (TemplateException e) {
-                e.printStackTrace();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
+        return "index---hello world";
 
-        String path = this.getClass().getClassLoader().getResource("/properties/a.properties").getPath();
-        InputStream  in = null;
-        OutputStream out = null;
-        try {
-            in = new FileInputStream(path);
-            out = new FileOutputStream(path,true);
-            Properties pro=new Properties();
-            try {
-               // pro.load(in);
-                pro.load(in);
-                pro.setProperty("bbb","tan");
-                pro.setProperty("bb1b","tan");
-                pro.store(out,"hahah1");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        InputStream  in1 = null;
-        try {
-            in1=new FileInputStream(new File(path));
-            Properties pro1=new Properties();
-            pro1.load(in1);
-            System.out.println(pro1);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return string;*/
     }
 }
